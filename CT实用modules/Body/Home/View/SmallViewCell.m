7103a74a3c6cc7417f07449f04f64412a3b00cbd
涛程 <429811196@qq.com>
2018-08-09 18:34:30 +0800

    小进度，不重要，丢弃要重写，布局方面

--- CT实用modules/Body/Home/View/SmallViewCell.m
+++ CT实用modules/Body/Home/View/SmallViewCell.m
@@ -8,7 +8,8 @@
 
 #import "SmallViewCell.h"
 @interface SmallViewCell()
-
+@property (nonatomic, strong) UIImageView *UserImaView;  //头像
+@property (nonatomic, strong) UILabel *NickNameLab;      //昵称
 @end
 @implementation SmallViewCell
 - (instancetype)initWithFrame:(CGRect)frame
@@ -21,6 +22,39 @@
 }
 
 - (void)loadUI{
-    self.backgroundColor = [UIColor redColor];
+    self.contentView.backgroundColor = [UIColor redColor];
+    [self.contentView addSubview:self.UserImaView];
+    [self.contentView addSubview:self.NickNameLab];
+    UIView *view = self.contentView;
+    
+    _UserImaView.sd_layout
+    .topSpaceToView(view, CONVER_VALUE(15))
+    .leftSpaceToView(view, CONVER_VALUE(12))
+    .widthIs(CONVER_VALUE(50))
+    .heightIs(CONVER_VALUE(50));
+    
+    _NickNameLab.sd_layout
+    .topSpaceToView(_UserImaView, CONVER_VALUE(8))
+    .leftEqualToView(_UserImaView)
+    .rightEqualToView(_UserImaView)
+    .heightIs(CONVER_VALUE(12));
+    
+    _UserImaView.backgroundColor = [UIColor lightGrayColor];
+    _NickNameLab.backgroundColor = [UIColor lightGrayColor];
+}
+
+
+#pragma mark - 懒加载
+- (UIImageView *)UserImaView{
+    if (!_UserImaView) {
+        _UserImaView = [[UIImageView alloc]init];
+    }
+    return _UserImaView;
+}
+- (UILabel *)NickNameLab{
+    if (!_NickNameLab) {
+        _NickNameLab = [[UILabel alloc]init];
+    }
+    return _NickNameLab;
 }
 @end
