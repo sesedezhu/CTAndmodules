2e6e42fae42e2170678cb85b3908d447801d82fe
涛程 <429811196@qq.com>
2018-10-25 17:11:00 +0800

    啊哈

--- CT实用modules/Body/My/Ctrls/ColorCtrl.m
+++ CT实用modules/Body/My/Ctrls/ColorCtrl.m
@@ -16,7 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(50, 100, kScreenWidth-100, 200)];
+    lab.numberOfLines = 0;
+    lab.text = @"这个太简单了，本人又太懒，就不创建示例了，对应类UIColor+Definition";
+    [self.view addSubview:lab];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
