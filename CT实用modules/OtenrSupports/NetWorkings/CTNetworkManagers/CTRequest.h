b3156ba63be62a804d94abe9b71e67965992ea29
xihasezhu <xihasezhu@xihasezhudeMac-mini.local>
2019-04-10 18:43:28 +0800

    网络处理

---
+++ CT实用modules/OtenrSupports/NetWorkings/CTNetworkManagers/CTRequest.h
@@ -0,0 +1,17 @@
+//
+//  CTRequest.h
+//  CT实用modules
+//
+//  Created by xihasezhu on 2019/4/10.
+//  Copyright © 2019 涛程. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTRequest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CT实用modules/OtenrSupports/NetWorkings/CTNetworkManagers/CTRequest.m
@@ -0,0 +1,13 @@
+//
+//  CTRequest.m
+//  CT实用modules
+//
+//  Created by xihasezhu on 2019/4/10.
+//  Copyright © 2019 涛程. All rights reserved.
+//
+
+#import "CTRequest.h"
+
+@implementation CTRequest
+
+@end
