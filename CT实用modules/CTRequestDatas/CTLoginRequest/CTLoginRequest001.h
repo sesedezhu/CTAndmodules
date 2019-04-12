98f340ffef612e7a94b3019265feff1bd8e4459e
xihasezhu <xihasezhu@xihasezhudeMac-mini.local>
2019-04-12 17:20:54 +0800

    找回/修改密码

--- CT实用modules/CTRequestDatas/CTLoginRequest/CTLoginRequest001.h
+++ CT实用modules/CTRequestDatas/CTLoginRequest/CTLoginRequest001.h
@@ -9,6 +9,18 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+typedef NS_ENUM(NSUInteger, CTVerificationCodeType) {
+    /** 获取登录验证码 */
+    CTVerificationCodeLogin,
+    /** 获取找回密码验证码 */
+    CTVerificationCodeBack,
+};
+typedef NS_ENUM(NSUInteger, CTLoginPasswordType) {
+    /** 修改密码 */
+    CTLoginPasswordModify,
+    /** 找回密码 */
+    CTLoginPasswordBack,
+};
 /** 请求成功的Block */
 typedef void(^CTSuccess)(id responseObject);
 /** 请求失败的Block */
@@ -23,8 +35,16 @@ typedef void(^CTFailed)(NSError *error);
  *  获取网络用户信息-手机密码登录
  */
 + (void)getRequestUserMobile:(NSString *)mobile MsgInfo:(NSString *)msgInfo success:(CTSuccess)success failure:(CTFailed)failure;
-//获取验证码
-+ (void)getVerificationCodeMobile:(NSString *)mobile success:(CTSuccess)success failure:(CTFailed)failure;
+/*
+ *  获取验证码
+ */
++ (void)getVerificationCodeMobile:(NSString *)mobile verificationCodeType:(CTVerificationCodeType)type success:(CTSuccess)success failure:(CTFailed)failure;
+/*
+ *  修/找回密码
+ */
++ (void)ModifyPasswordsInMobile:(NSString *)mobile MsgInfo:(NSString *)msgInfo password:(NSString *)password loginPasswordType:(CTLoginPasswordType)type success:(CTSuccess)success failure:(CTFailed)failure;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- CT实用modules/CTRequestDatas/CTLoginRequest/CTLoginRequest001.m
+++ CT实用modules/CTRequestDatas/CTLoginRequest/CTLoginRequest001.m
@@ -15,7 +15,8 @@
 #define NLWlogin                           @"/userlogin/login"
 //4.3.1验证码接口
 #define NLWSendDps                         @"/userlogin/sendDps"
-
+//4.12.1用户修改密码接口(包括找回密码)
+#define NLWModifyUserPwd                   @"/userpwd/modifyUserPwd"
 //用户资料
 #define UserPlists @"User.plist"
 @interface CTLoginRequest001()
@@ -67,10 +68,21 @@
     }];
 }
 //获取验证码
-+ (void)getVerificationCodeMobile:(NSString *)mobile success:(CTSuccess)success failure:(CTFailed)failure{
++ (void)getVerificationCodeMobile:(NSString *)mobile verificationCodeType:(CTVerificationCodeType)type success:(CTSuccess)success failure:(CTFailed)failure{
+    NSString *remarkInfo;
+    switch (type) {
+        case CTVerificationCodeLogin:
+            remarkInfo = @"4";
+            break;
+        case CTVerificationCodeBack:
+            remarkInfo = @"3";
+            break;
+        default:
+            break;
+    }
     NSMutableDictionary * messageDic = [[NSMutableDictionary alloc]init];
     [messageDic setObject:mobile forKey:@"mobileNo"];
-    [messageDic setObject:@"4" forKey:@"remarkInfo"];
+    [messageDic setObject:remarkInfo forKey:@"remarkInfo"];
     NSString *md5Str = [CTStringTool stringWithDict:messageDic];
     NSString *md5 = [CTMD5Tool MD5ForUpper32Bate:md5Str];
     [messageDic setObject:md5 forKey:@"sign"];
@@ -83,6 +95,45 @@
         failure ? failure(error) : nil;
     }];
 }
+/*
+ *  修/找回密码
+ */
++ (void)ModifyPasswordsInMobile:(NSString *)mobile MsgInfo:(NSString *)msgInfo password:(NSString *)password loginPasswordType:(CTLoginPasswordType)type success:(CTSuccess)success failure:(CTFailed)failure{
+    NSString *modifyFlag;
+    switch (type) {
+        case CTLoginPasswordModify:
+            modifyFlag = @"1";
+            break;
+        case CTLoginPasswordBack:
+            modifyFlag = @"2";
+            break;
+        default:
+            break;
+    }
+    NSMutableDictionary * messageDic = [[NSMutableDictionary alloc]init];
+    [messageDic setObject:mobile forKey:@"mobileNo"];
+    [messageDic setObject:msgInfo forKey:@"msgInfo"];
+    [messageDic setObject:password forKey:@"password"];
+    [messageDic setObject:modifyFlag forKey:@"modifyFlag"];
+    [messageDic setObject:@"3" forKey:@"remarkInfo"];
+    NSString *md5Str = [CTStringTool stringWithDict:messageDic];
+    NSString *md5 = [CTMD5Tool MD5ForUpper32Bate:md5Str];
+    [messageDic setObject:md5 forKey:@"sign"];
+    
+    [CTRequest startPostConnectionWithAddress:RequestPrefix Path:NLWModifyUserPwd parameter:messageDic CaCha:NO cacha:^(id  _Nonnull responseCache) {
+        
+    } success:^(id  _Nonnull responseObject) {
+        success ? success(responseObject) : nil;
+    } failure:^(NSError * _Nonnull error) {
+        failure ? failure(error) : nil;
+    }];
+}
+
+
+
+
+
+
 //请求成功写入用户信息
 + (void)writeUserDataInPlist:(id)responseObject{
     NSString *errCode = [responseObject objectForKey:@"errCode"];
@@ -91,5 +142,4 @@
         [PlistManager isFilePlistName:UserPlists loadData:data];
     }
 }
-
 @end
