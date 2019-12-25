ec1f8879bdd6e4ba60223e4fa0e6b25bfe711661
xihasezhu <xihasezhu@xihasezhudeMac-mini.local>
2019-12-25 11:37:06 +0800

    优化wkwebview

--- CT实用modules/CTFramework/CTViews/CTUIWKWebviews/ToolWKWebView01.h
+++ CT实用modules/CTFramework/CTViews/CTUIWKWebviews/ToolWKWebView01.h
@@ -11,8 +11,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ToolWKWebView01 : UIView
-- (void)loadWKWebViewWithURL:(id)url;
-- (void)loadloadWKWebViewHTMLString:(NSString *)htmlString;
+
+// 对声明参数添加属性（__nullable）时参数可传nil，传（__nonnull）时参数不能为空
+- (void)loadWKWebViewWithStringURL:(NSString *)urlString Type:(NSString *__nullable)types;
+- (void)loadloadWKWebViewHTMLString:(NSString *)htmlString Type:(NSString *__nullable)types;
 @end
 
 NS_ASSUME_NONNULL_END
--- CT实用modules/CTFramework/CTViews/CTUIWKWebviews/ToolWKWebView01.m
+++ CT实用modules/CTFramework/CTViews/CTUIWKWebviews/ToolWKWebView01.m
@@ -10,7 +10,7 @@
 #import <WebKit/WebKit.h>
 @interface ToolWKWebView01()<WKUIDelegate,WKNavigationDelegate>
 @property(nonatomic,strong)WKWebView *WKView;
-@property (nonatomic, assign) BOOL isHtmlString;//判断加载方式
+@property(nonatomic,copy)NSString *WKTypes;//不同进入入口类型区分
 @end
 @implementation ToolWKWebView01
 - (instancetype)initWithFrame:(CGRect)frame {
@@ -32,7 +32,7 @@
     .rightSpaceToView(view, 0)
     .bottomSpaceToView(view, 0);
     
-    [self loadWebViewWithURL:@"https://www.jianshu.com/p/416b84d87105"];
+    [self loadWKWebViewWithStringURL:@"https://www.jianshu.com/p/416b84d87105" Type:nil];
 
     
 }
@@ -50,10 +50,10 @@
 // 加载完成
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
     NSLog(@"加载完成");
-  if (_isHtmlString) {
-      //修改字体大小 240%
-      [webView evaluateJavaScript:@"document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '320%'"completionHandler:nil];
-  }
+// if ([_WKTypes isEqualToString:@"ToolWebview01"] || [_WKTypes isEqualToString:@"WF_QLPrivacyVCViewController"]) {
+//     //修改字体大小 240%
+//     [webView evaluateJavaScript:@"document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '320%'"completionHandler:nil];
+// }
 }
 // 加载失败
 - (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
@@ -62,39 +62,20 @@
 }
 
 #pragma mark - 加载wkwebView
-- (void)loadWebViewWithURL:(id)url {
-    _isHtmlString = NO;
-    NSURL *loadURL = nil;
+- (void)loadWKWebViewWithStringURL:(NSString *)urlString Type:(NSString *)types {
+   _WKTypes = types;
     // 对url进行检查
-    if (!url) {
+    if (!urlString) {
         return;
     }
-    if ([url hasSuffix:@"html"]) { // 本地html
-        NSString *path = [[NSBundle mainBundle] pathForResource:url ofType:nil]; // 如果Bundle中没有该文件会返回nil
-        if (!path) {
-            NSLog(@"未查找到文件: %@",url);
-            return;
-        }
-        loadURL = [NSURL fileURLWithPath:path];
-    }
-    else if ([url isKindOfClass:[NSURL class]]) { // URL
-        loadURL = url;
-    }
-    else if ([url isKindOfClass:[NSString class]]) { // URL Stirng
-        [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        loadURL = [NSURL URLWithString:url];
-    }
-    else {
-        NSLog(@"格式错误：url- %@",url);
-    }
     // 加载web页面
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:loadURL];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
     [self.WKView loadRequest:request];
     
 }
 #pragma mark - 特殊数据加载HTMLString
-- (void)loadloadWKWebViewHTMLString:(NSString *)htmlString{
-    _isHtmlString = YES;
+- (void)loadloadWKWebViewHTMLString:(NSString *)htmlString Type:(NSString *)types{
+    _WKTypes = types;
     // 对url进行检查
     if (!htmlString) {
         return;
